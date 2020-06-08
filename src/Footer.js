31a52fd14a092cd498e63781874e3da27c9a9f08
杨晨珑 <yangchenlong@yangchenlongdeMacBook-Pro.local>
2020-06-08 19:35:09 -0400

    footer

--- src/Footer.js
+++ src/Footer.js
@@ -4,7 +4,8 @@ export default function Footer() {
   return (
     <div className="App-footer">
       <div>Designed and developed by Jacky C. Yang</div>
-      <div>Main page image created by: hyung86 on experiencefilm.com</div>
+      <div>Data from Studio Ghibli API, OMDb API, and IMDb API</div>
+      <div>Home page image by: hyung86 on experiencefilm.com</div>
     </div>
   )
 }
